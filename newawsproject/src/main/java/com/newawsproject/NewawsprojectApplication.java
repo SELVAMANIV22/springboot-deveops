598b96983f513677db76232fcfe40a75349f5df7
SELVAMANI <SELVAMANI@DESKTOP-N8H31BH>
2021-06-09 15:55:02 +0530

    first commit

---
+++ newawsproject/src/main/java/com/newawsproject/NewawsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.newawsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewawsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewawsprojectApplication.class, args);
+	}
+
+}
